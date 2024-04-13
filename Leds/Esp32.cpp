756d6dce1bcedfda0f85f1f28c8ae885834ca7da
Belencita <beckiar23@gmail.com>
2024-04-12 20:55:34 -0400

    The esp 32 class is created

---
+++ Leds/Esp32.cpp
@@ -0,0 +1,14 @@
+#include"Esp32.h"
+Led Esp32::leds[LEDS_COUNT]; 
+Esp32::Esp32(){
+
+}
+void Esp32::initializeLeds(){
+    for(int i=0;i<LEDS_COUNT;i++)
+      leds[i] = Led(ledPins[i]);
+}
+Led * Esp32::getLeds(){
+  return leds;
+}
+Esp32::~Esp32(){
+}
---
+++ Leds/Esp32.h
@@ -0,0 +1,15 @@
+#ifndef ESP32_H
+#define ESP32_H
+#include "constants.h"
+#include "Led.h"
+class Esp32{
+  private:
+    static Led leds[LEDS_COUNT];
+  public:
+    Esp32();
+    static Led * getLeds();
+    void initializeLeds();
+    ~Esp32();
+
+};
+#endif
