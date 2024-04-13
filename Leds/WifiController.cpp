0822185fce8cb00e922cd0285865af7a49d8e7f6
susan-cespedes <susan.cespedes@ucb.edu.bo>
2024-04-12 20:52:26 -0400

    the wifi controller class is created

---
+++ Leds/WifiController.cpp
@@ -0,0 +1,21 @@
+#include"WifiController.h"
+
+WifiController::WifiController(char * WifiSsid, char * WifiPassword):wifiClient(){
+  this->WifiSsid=WifiSsid;
+  this->WifiPassword=WifiPassword;
+
+}
+
+void WifiController::connectToWifi(){
+  WiFi.begin(WifiSsid,WifiPassword);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(STAND_BY_DELAY);
+    Serial.print(".");
+  }
+  Serial.println(" Connected to" + String(WifiSsid));
+}
+WiFiClient & WifiController::getWiFiClient(){
+  return wifiClient;
+}
+WifiController::~WifiController(){
+}
