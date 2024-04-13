25419e2ccd389452e0f5c00ccd0181751406c62e
987sebasBeller <sebastianbeller76@gmail.com>
2024-04-12 20:58:10 -0400

    The main.ino file is created

---
+++ Leds/main.ino
@@ -0,0 +1,23 @@
+#include "Esp32.h"
+#include<Arduino.h>
+#include"WifiController.h"
+#include"constants.h"
+#include "MqttClientController.h"
+Esp32 esp;
+WifiController wifiController(WIFI_SSID,WIFI_PASS);
+MqttClientController  mqttController(MQTT_BROKER_HOST,MQTT_BROKER_PORT); 
+
+void setup()
+{ 
+  Serial.begin(TRANSMISSION_SPEED);
+  wifiController.connectToWifi();
+  esp.initializeLeds();
+  mqttController.setServerForMqtt(wifiController);
+  mqttController.connectWithClientId(CLIENT_ID);
+  mqttController.subscribeToTheTopics();
+}
+
+void loop()
+{
+  mqttController.startListening();
+}
