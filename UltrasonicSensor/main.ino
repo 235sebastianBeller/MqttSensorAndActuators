6cdf62af96d16a005837b8794fdc40ba14c3d0ca
987sebasBeller <sebastianbeller76@gmail.com>
2024-04-12 20:41:22 -0400

    The main.ino file is added

---
+++ UltrasonicSensor/main.ino
@@ -0,0 +1,21 @@
+#include "UltrasonicSensor.h"
+#include "WifiController.h"
+#include "MqttClientController.h"
+#include "constants.h"
+
+WifiController wifiController(WIFI_SSID,WIFI_PASS);
+MqttClientController  mqttController(MQTT_BROKER_HOST,MQTT_BROKER_PORT); 
+UltrasonicSensor ultrasonicSensor(TRIGGER_PIN,ECHO_PIN);
+
+void setup() {
+  Serial.begin(TRANSMISSION_SPEED);
+  wifiController.connectToWifi();
+  mqttController.setServerForMqtt(wifiController);
+  mqttController.connectWithClientId(CLIENT_ID);
+}
+
+int cm=0;
+void loop() {
+  cm=ultrasonicSensor.getTheShortestDistance(SAMPLE_SIZE);
+  mqttController.publishOnTopic(String(cm),PUBLISH_TOPIC);
+}
