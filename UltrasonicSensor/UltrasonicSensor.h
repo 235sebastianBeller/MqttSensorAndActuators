14e86bcbe3801242a4ed242537a51de471dbc969
susan-cespedes <susan.cespedes@ucb.edu.bo>
2024-04-12 20:37:13 -0400

    the ultrasonicSensor class is modified

---
+++ UltrasonicSensor/UltrasonicSensor.h
@@ -0,0 +1,16 @@
+#ifndef ULTRASONICSENSOR_H
+#define ULTRASONICSENSOR_H
+#include <Arduino.h>
+
+class UltrasonicSensor {
+private:
+    byte triggerPin;
+    byte echoPin; 
+public:
+    UltrasonicSensor(byte triggerPin,byte echoPin);
+    long  readDistance();
+    int getTheShortestDistance(byte sampleSize);
+    ~UltrasonicSensor();
+};
+
+#endif
