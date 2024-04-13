c6b83cbd8d66e17d91e57fe00a67ef78cb58b442
susan-cespedes <susan.cespedes@ucb.edu.bo>
2024-04-12 20:51:32 -0400

    the leds class is modified

---
+++ Leds/Led.cpp
@@ -0,0 +1,36 @@
+#include"Led.h"
+Led::Led(int ledPin){
+  this->ledPin=ledPin;
+  pinMode(ledPin, OUTPUT);
+}
+Led::Led(){
+}
+void Led::turnOn(){
+    digitalWrite(ledPin,HIGH);
+}
+void Led::turnOff(){
+    digitalWrite(ledPin,LOW);
+}
+bool Led::isLedShouldBeOn(String message){
+  return message == "LED_ON"; 
+}
+void Led::changeState(String message){
+    if (isLedShouldBeOn(message)) 
+      turnOn();
+    else  
+      turnOff();
+    
+}
+int Led::getIdLedFromMessage(String message){
+  int id =0,i=message.length()-1,pot=1;
+    Serial.println(message);
+
+    Serial.println(message[i]);
+  while(message[i]>='0' &&message[i]<='9' ){
+    id+=pot*(message[i--]-'0');
+    pot*=10;
+  }
+  return id;
+}
+Led::~Led(){
+}
---
+++ Leds/Led.h
@@ -0,0 +1,19 @@
+#ifndef LED_H
+#define LED_H
+#include<Arduino.h>
+#include <sstream>
+using namespace std;
+class Led {
+  private:
+    int ledPin;
+  public:
+  Led();
+  Led(int ledPin);
+  void turnOn();
+  void turnOff();
+  void changeState(String message);
+  bool isLedShouldBeOn(String message);
+  static int getIdLedFromMessage(String message);
+  ~Led();
+};
+#endif
